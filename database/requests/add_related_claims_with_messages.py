d68602b0ddf55af0521aeb22b97edef403763f4c
Alexander Choliy <alexander.choliy@gmail.com>
2025-01-31 11:17:42 +0300

    improve add_related_claims sql query

--- database/requests/add_related_claims_with_messages.py
+++ database/requests/add_related_claims_with_messages.py
@@ -18,7 +18,7 @@ def add_related_claims_with_messages(
         constant_text,
         time_stamp
     )
-    SELECT
+    SELECT DISTINCT
         mc.message_id,
         2070 AS constant_type,
         cl.claim_number AS constant_text,
@@ -28,18 +28,21 @@ def add_related_claims_with_messages(
     WHERE
         mc.constant_type = 2060
         AND cl.claim_number IS NOT NULL
-        AND mc.id NOT IN (
-            SELECT id
-            FROM messages_constants
-            WHERE constant_type = 2070
-        )
         {personal_area_id_filter}
         {declarant_id_filter}
         AND NOT EXISTS (
             SELECT 1
             FROM messages_constants AS mc2
-            WHERE mc2.message_id = mc.message_id
-            AND mc2.constant_type = 2070
-            AND mc2.constant_text = cl.claim_number
+            WHERE
+                mc2.message_id = mc.message_id
+                AND mc2.constant_type = 2070
+                AND mc2.constant_text = cl.claim_number
+        )
+        AND NOT EXISTS (
+            SELECT 1
+            FROM messages_constants AS mc3
+            WHERE
+                mc3.message_id = mc.message_id
+                AND mc3.constant_type = 2070
         );
     '''.strip())
